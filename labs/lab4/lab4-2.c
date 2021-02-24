8358b145e22e4e4c32af73906ada5acb337a4fc0
Måns Alskog <manal270@student.liu.se>
2021-02-24 13:34:49 +0100

    Done with lab 4 parts 1 and 2

--- labs/lab4/lab4-2.c
+++ labs/lab4/lab4-2.c
@@ -11,6 +11,9 @@
 #include "LittleOBJLoader.h"
 #include "LoadTGA.h"
 
+const int WIN_WIDTH = 600;
+const int WIN_HEIGHT = 600;
+
 mat4 projectionMatrix;
 
 Model* GenerateTerrain(TextureData *tex)
@@ -31,7 +34,7 @@ Model* GenerateTerrain(TextureData *tex)
 // Vertex array. You need to scale this properly
 			vertexArray[x + z * tex->width] = SetVector(
 					x / 1.0,
-					tex->imageData[(x + z * tex->width) * (tex->bpp/8)] / 100.0,
+					tex->imageData[(x + z * tex->width) * (tex->bpp/8)] / 10.0,
 					z / 1.0);
 // Normal vectors. You need to calculate these.
 			normalArray[x + z * tex->width] = SetVector(0.0, 1.0, 0.0);
@@ -97,14 +100,14 @@ void init(void)
 
 // Load terrain data
 
-	LoadTGATextureData("44-terrain.tga", &ttex);
+	LoadTGATextureData("fft-terrain.tga", &ttex);
 	tm = GenerateTerrain(&ttex);
 	printError("init terrain");
 }
 
 const vec3 up_vector = {0.0, 1.0, 0.0};
-vec3 view_target;
-vec3 view_pos;
+vec3 view_target = {2, 0, 2};
+vec3 view_pos = {0, 5, 8};
 
 void display(void)
 {
@@ -119,11 +122,7 @@ void display(void)
 
 	// Build matrix
 
-	vec3 cam = {0, 5, 8};
-	vec3 lookAtPoint = {2, 0, 2};
-	camMatrix = lookAt(cam.x, cam.y, cam.z,
-					   lookAtPoint.x, lookAtPoint.y, lookAtPoint.z,
-					   0.0, 1.0, 0.0);
+	camMatrix = lookAtv(view_pos, view_target, up_vector);
 	modelView = IdentityMatrix();
 	total = Mult(camMatrix, modelView);
 	glUniformMatrix4fv(glGetUniformLocation(program, "mdlMatrix"), 1, GL_TRUE, total.m);
@@ -136,15 +135,102 @@ void display(void)
 	glutSwapBuffers();
 }
 
+
+void handleFlyingControls(float delta) {
+	const float MOVE_SPEED = 50.0f;
+	const float LOOK_SPEED = 200.0f;
+	// Fly around in the world, update view matrix for next frame
+	const vec3 forward_vector = Normalize(VectorSub(view_target, view_pos));
+	// cross product of two unit vectors is also unit
+	const vec3 right_vector = Normalize(CrossProduct(forward_vector, up_vector));
+	// Forward and backward
+	if (glutKeyIsDown('w')) {
+		view_pos = VectorAdd(view_pos, ScalarMult(forward_vector, delta * MOVE_SPEED));
+	}
+	if (glutKeyIsDown('s')) {
+		view_pos = VectorSub(view_pos, ScalarMult(forward_vector, delta * MOVE_SPEED));
+	}
+	// Strafing left and right
+	if (glutKeyIsDown('d')) {
+		view_pos = VectorAdd(view_pos, ScalarMult(right_vector, delta * MOVE_SPEED));
+	}
+	if (glutKeyIsDown('a')) {
+		view_pos = VectorSub(view_pos, ScalarMult(right_vector, delta * MOVE_SPEED));
+	}
+	// Up and down
+	if (glutKeyIsDown(' ')) {
+		view_pos = VectorAdd(view_pos, ScalarMult(up_vector, delta * MOVE_SPEED));
+	}
+	if (glutKeyIsDown('c')) {
+		view_pos = VectorSub(view_pos, ScalarMult(up_vector, delta * MOVE_SPEED));
+	}
+	// Put view target 100 units in front of view pos
+	view_target = VectorAdd(view_pos, ScalarMult(forward_vector, 100.0f));
+	// Turning
+	if (glutKeyIsDown(GLUT_KEY_RIGHT)) {
+		view_target = VectorAdd(view_target, ScalarMult(right_vector, delta * LOOK_SPEED));
+	}
+	if (glutKeyIsDown(GLUT_KEY_LEFT)) {
+		view_target = VectorSub(view_target, ScalarMult(right_vector, delta * LOOK_SPEED));
+	}
+	if (glutKeyIsDown(GLUT_KEY_UP)) {
+		view_target = VectorAdd(view_target, ScalarMult(up_vector, delta * LOOK_SPEED));
+	}
+	if (glutKeyIsDown(GLUT_KEY_DOWN)) {
+		view_target = VectorSub(view_target, ScalarMult(up_vector, delta * LOOK_SPEED));
+	}
+}
+
+void handleMouseLook(float delta, int dx, int dy) {
+	const int MIN_PIXELS = 5;
+	const float LOOK_SPEED = 150.0f;
+	const vec3 forward_vector = Normalize(VectorSub(view_target, view_pos));
+	const vec3 right_vector = Normalize(CrossProduct(forward_vector, up_vector));
+	// Mouse look
+	if (dx > MIN_PIXELS) {
+		view_target = VectorAdd(view_target, ScalarMult(right_vector, delta * LOOK_SPEED));
+	}
+	if (dx < -MIN_PIXELS) {
+		view_target = VectorSub(view_target, ScalarMult(right_vector, delta * LOOK_SPEED));
+	}
+	if (dy < -MIN_PIXELS) {
+		view_target = VectorAdd(view_target, ScalarMult(up_vector, delta * LOOK_SPEED));
+	}
+	if (dy > MIN_PIXELS) {
+		view_target = VectorSub(view_target, ScalarMult(up_vector, delta * LOOK_SPEED));
+	}
+}
+
+GLfloat prev_t = 0.0;
+int paused = 0;
+
 void timer(int i)
 {
 	glutTimerFunc(20, &timer, i);
+    GLfloat t = (GLfloat) glutGet(GLUT_ELAPSED_TIME) / 1000.0f;
+	handleFlyingControls(t - prev_t);
+	prev_t = t;
 	glutPostRedisplay();
 }
 
 void mouse(int x, int y)
 {
-	// printf("%d %d\n", x, y);
+	if (!paused) {
+		GLfloat t = (GLfloat) glutGet(GLUT_ELAPSED_TIME) / 1000.0f;
+		handleMouseLook(t - prev_t, x - WIN_WIDTH / 2, y - WIN_HEIGHT / 2);
+		glutWarpPointer(WIN_WIDTH / 2, WIN_HEIGHT / 2);
+	}
+}
+
+void keyboard(unsigned char key, int x, int y) {
+	if (key == GLUT_KEY_ESC) {
+		paused = !paused;
+		if (paused) {
+			glutShowCursor();
+		} else {
+			glutHideCursor();
+		}
+	}
 }
 
 int main(int argc, char **argv)
@@ -152,12 +238,14 @@ int main(int argc, char **argv)
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
 	glutInitContextVersion(3, 2);
-	glutInitWindowSize (600, 600);
-	glutCreateWindow ("TSBK07 Lab 4");
+	glutInitWindowSize(WIN_WIDTH, WIN_HEIGHT);
+	glutCreateWindow("TSBK07 Lab 4");
 	glutDisplayFunc(display);
-	init ();
+	glutKeyboardFunc(keyboard);
+	init();
 	glutTimerFunc(20, &timer, 0);
 
+	glutHideCursor();
 	glutPassiveMotionFunc(mouse);
 
 	glutMainLoop();
